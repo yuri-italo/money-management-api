35fb0cd11502f7247718d53a71cc630a4b6d39e0
yuri-italo <yuri.italo94@gmail.com>
2022-06-19 14:40:55 -0300

    ExpenseController is now responsible for returning Responses Entities.

--- src/main/java/br/com/alura/moneymanagementapi/controller/ExpenseController.java
+++ src/main/java/br/com/alura/moneymanagementapi/controller/ExpenseController.java
@@ -1,16 +1,22 @@
 package br.com.alura.moneymanagementapi.controller;
 
+import br.com.alura.moneymanagementapi.dto.ExpenseDto;
+import br.com.alura.moneymanagementapi.dto.ExpenseWithoutIdDto;
 import br.com.alura.moneymanagementapi.form.ExpenseForm;
+import br.com.alura.moneymanagementapi.model.Expense;
 import br.com.alura.moneymanagementapi.service.ExpenseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/expenses")
@@ -28,38 +34,81 @@ public class ExpenseController {
     @Operation(summary = "Create a new expense.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> save(@RequestBody @Valid ExpenseForm expenseForm, UriComponentsBuilder uriBuilder) {
-        return expenseService.save(expenseForm,uriBuilder);
+        if (expenseService.expenseAlreadyExists(expenseForm))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Monthly expense already exists.");
+
+        Expense savedExpense = expenseService.save(expenseForm);
+
+        return ResponseEntity.created(uriBuilder.path("/expenses/{id}").buildAndExpand(savedExpense.getId()).toUri()).body(new ExpenseDto(savedExpense));
     }
+
     @GetMapping
     @Operation(summary = "Return a list of expenses.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> listAll(@RequestParam(required = false) String description) {
-      return expenseService.listAll(description);
+        List<Expense> list = expenseService.listAll(description);
+
+        if (list.isEmpty())
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(ExpenseDto.convertManyToDto(list));
     }
+
     @GetMapping("/{id}")
     @Operation(summary = "Get an expense by id.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> getById(@PathVariable Long id) {
-        return expenseService.getById(id);
+        Optional<Expense> optional = expenseService.findById(id);
+
+        if (optional.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(new ExpenseWithoutIdDto(optional.get()));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense does not exist.");
     }
+
     @GetMapping("/{year}/{month}")
     @Operation(summary = "Get an expense by month")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> getByMonth(@PathVariable int year, @PathVariable int month) {
-        return expenseService.getByMonth(year,month);
+        List<Expense> list = expenseService.findByMonth(year, month);
+
+        if (list.isEmpty())
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(ExpenseWithoutIdDto.convertManyToDto(list));
     }
+
     @PutMapping("/{id}")
     @Transactional
     @Operation(summary = "Update one or more expense fields by id.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> updateById(@PathVariable Long id, @RequestBody @Valid ExpenseForm expenseForm) {
-        return expenseService.updateById(id,expenseForm);
+        Optional<Expense> optional = expenseService.findById(id);
+
+        if (optional.isPresent()) {
+            if (expenseService.expenseAlreadyExists(expenseForm) && expenseService.isNotModified(optional.get(),expenseForm))
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Monthly expense already exists.");
+
+            Expense updatedExpense = expenseService.update(id, expenseForm);
+
+            return ResponseEntity.status(HttpStatus.OK).body(new ExpenseWithoutIdDto(updatedExpense));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense does not exist.");
     }
+
     @DeleteMapping("/{id}")
     @Transactional
     @Operation(summary = "Delete an expense by id.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
-        return expenseService.deleteById(id);
+        Optional<Expense> optional = expenseService.findById(id);
+
+        if (optional.isPresent()) {
+            expenseService.delete(optional.get());
+            return ResponseEntity.status(HttpStatus.OK).body("Expense deleted.");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense does not exist.");
     }
 }
--- src/main/java/br/com/alura/moneymanagementapi/service/ExpenseService.java
+++ src/main/java/br/com/alura/moneymanagementapi/service/ExpenseService.java
@@ -1,16 +1,11 @@
 package br.com.alura.moneymanagementapi.service;
 
-import br.com.alura.moneymanagementapi.dto.ExpenseDto;
-import br.com.alura.moneymanagementapi.dto.ExpenseWithoutIdDto;
 import br.com.alura.moneymanagementapi.dto.MonthlyExpenseByCategoryDto;
 import br.com.alura.moneymanagementapi.form.ExpenseForm;
 import br.com.alura.moneymanagementapi.model.Category;
 import br.com.alura.moneymanagementapi.model.Expense;
 import br.com.alura.moneymanagementapi.repository.ExpenseRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -25,21 +20,15 @@ public class ExpenseService {
         this.expenseRepository = expenseRepository;
     }
 
-    public ResponseEntity<?> save(ExpenseForm expenseForm, UriComponentsBuilder uriBuilder) {
-        if (expenseAlreadyExists(expenseForm))
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Monthly expense already exists.");
-
-        Expense expense = new Expense(expenseForm);
-        expenseRepository.save(expense);
-
-        return ResponseEntity.created(uriBuilder.path("/expenses/{id}").buildAndExpand(expense.getId()).toUri()).body(new ExpenseDto(expense));
+    public Expense save(ExpenseForm expenseForm) {
+        return expenseRepository.save(new Expense(expenseForm));
     }
 
-    private boolean expenseAlreadyExists(ExpenseForm expenseForm) {
+    public boolean expenseAlreadyExists(ExpenseForm expenseForm) {
         return expenseRepository.findByDescriptionAndDate(expenseForm.getDescription(),expenseForm.getDate().getYear(),expenseForm.getDate().getMonthValue());
     }
 
-    public ResponseEntity<?> listAll(String description) {
+    public List<Expense> listAll(String description) {
         List<Expense> expenseList;
 
         if (description == null)
@@ -47,48 +36,26 @@ public class ExpenseService {
         else
             expenseList = expenseRepository.findByDescriptionContainingIgnoreCase(description);
 
-        if (expenseList.isEmpty())
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(ExpenseDto.convertManyToDto(expenseList));
+        return expenseList;
     }
 
-    public ResponseEntity<?> getById(Long id) {
-        Optional<Expense> optional = expenseRepository.findById(id);
-
-        if (optional.isPresent())
-            return ResponseEntity.status(HttpStatus.OK).body(new ExpenseWithoutIdDto(optional.get()));
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense does not exist.");
+    public Optional<Expense> findById(Long id) {
+        return expenseRepository.findById(id);
     }
 
-    public ResponseEntity<?> getByMonth(int year, int month) {
-        List<Expense> expenseList = expenseRepository.findByMonth(year, month);
-
-        if (expenseList.isEmpty())
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(ExpenseWithoutIdDto.convertManyToDto(expenseList));
+    public List<Expense> findByMonth(int year, int month) {
+        return expenseRepository.findByMonth(year, month);
     }
 
-    public ResponseEntity<?> updateById(Long id, ExpenseForm expenseForm) {
-        Optional<Expense> optional = expenseRepository.findById(id);
-
-        if (optional.isPresent()) {
-            if (expenseAlreadyExists(expenseForm) && isNotModified(optional.get(),expenseForm))
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("Monthly expense already exists.");
-
-            return ResponseEntity.status(HttpStatus.OK).body(updateFields(optional.get(),expenseForm));
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense does not exist.");
+    public Expense update(Long id, ExpenseForm expenseForm) {
+        return updateFields(expenseRepository.getById(id),expenseForm);
     }
 
-    private boolean isNotModified(Expense expense, ExpenseForm expenseForm) {
+    public boolean isNotModified(Expense expense, ExpenseForm expenseForm) {
         return expense.getValue().compareTo(expenseForm.getValue()) == 0 && expenseForm.getCategory() == null;
     }
 
-    private ExpenseWithoutIdDto updateFields(Expense expense, ExpenseForm expenseForm) {
+    private Expense updateFields(Expense expense, ExpenseForm expenseForm) {
         expense.setDescription(expenseForm.getDescription());
         expense.setValue(expenseForm.getValue());
         expense.setDate(expenseForm.getDate());
@@ -96,18 +63,11 @@ public class ExpenseService {
         if (expenseForm.getCategory() != null)
             expense.setCategory(expenseForm.getCategory());
 
-        return new ExpenseWithoutIdDto(expense);
+        return expense;
     }
 
-    public ResponseEntity<?> deleteById(Long id) {
-        Optional<Expense> optional = expenseRepository.findById(id);
-
-        if (optional.isPresent()) {
-            expenseRepository.deleteById(id);
-            return ResponseEntity.status(HttpStatus.OK).body("Expense deleted.");
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense does not exist.");
+    public void delete(Expense expense) {
+        expenseRepository.delete(expense);
     }
 
     public MonthlyExpenseByCategoryDto getExpenseByCategory(Category category) {
